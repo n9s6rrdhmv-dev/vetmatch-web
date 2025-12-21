b7fcb4af0cb570998af10188ba66a32b6b733329
n9s6rrdhmv-dev <n9s6rrdhmv@privaterelay.appleid.com>
2025-12-21 13:56:11 -0600

    Delete app

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>VetMatch</h1>
-      <p>MVP en construcción (MX + US).</p>
-    </main>
-  );
-}
