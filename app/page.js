ce6634cad7a9df981c03322bf940047d936068dd
n9s6rrdhmv-dev <n9s6rrdhmv@privaterelay.appleid.com>
2025-12-21 13:56:58 -0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>VetMatch</h1>
+      <p>MVP en construcción (MX + US).</p>
+    </main>
+  );
+}
